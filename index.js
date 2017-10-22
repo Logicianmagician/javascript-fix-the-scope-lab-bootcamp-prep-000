d03b5c8d5e73cc446c802925dd0afc5408f56fdb
Andrew Kairys <andrewkairys@gmail.com>
2017-10-22 17:22:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,11 +19,11 @@ function add2(n) {
   const two = Math.floor(Math.random() * 1000)
 }
 
-var funkyFunction() = function() {
-  return function(){
+var funkyFunction = function() {
+  return function() {
     return "FUNKY!"
 }
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction()
+var 'theFunk' = funkyFunction()
